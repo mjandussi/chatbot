71bed63488611be9f7ccab2930313aa52a767a34
mjandussi <112095881+mjandussi@users.noreply.github.com>
2024-12-24 09:49:20 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,9 @@ from openai import OpenAI
 # Show title and description.
 st.title("💬 Chatbot")
 st.write(
-    "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-    "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
+    "Este é um chatbot simples que utiliza o modelo GPT-3.5 da OpenAI para gerar respostas. "
+    "Para usar este aplicativo, você precisa fornecer uma chave de API da OpenAI, que você pode obter [aqui](https://platform.openai.com/account/api-keys). "
+    "Você também pode aprender como construir este aplicativo passo a passo [seguindo nosso tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
 )
 
 # Ask user for their OpenAI API key via `st.text_input`.
@@ -14,7 +14,7 @@ st.write(
 # via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
 openai_api_key = st.text_input("OpenAI API Key", type="password")
 if not openai_api_key:
-    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
+    st.info("Coloque sua chave da OpenAI API para continuar.", icon="🗝️")
 else:
 
     # Create an OpenAI client.
@@ -32,7 +32,7 @@ else:
 
     # Create a chat input field to allow the user to enter a message. This will display
     # automatically at the bottom of the page.
-    if prompt := st.chat_input("What is up?"):
+    if prompt := st.chat_input("Qual sua pergunta ou dúvida?"):
 
         # Store and display the current prompt.
         st.session_state.messages.append({"role": "user", "content": prompt})
